d6fb36c31c91e4e065646a5251952621147437d3
Jekipung <jung103172@gmail.com>
2026-02-01 21:11:14 +0900

    test

---
+++ ProjectCounseling/vlog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+   path('', views.post_list, name='post_list'),
+   path("new/", views.PostCreate.as_view(), name="post_create"),
+   path("<int:pk>", views.post_detail, name="post_detail"),
+   path("<int:pk>/edit/",views.PostUpdate.as_view(), name="post_update"),
+   path("<int:pk>/delete/", views.post_delete, name = 'post_delete'),
+]
+
+
+
+
---
+++ ProjectCounseling/vlog/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView
+from .models import Post
+from .forms import PostForm
+def post_list(request):
+   posts = Post.objects.order_by('-id')
+   return render(request, 'vlog/post_list.html', {'posts' : posts})
+
+def post_delete(request, pk):
+   post = get_object_or_404(Post, pk = pk)
+
+   if request.method == "GET":
+      return render(request, "vlog/post_confirm_delete.html", {"post": post})
+
+   if request.method == 'POST':
+      post.delete()
+      return redirect("post_list")
+
+def post_detail(request, pk):
+   post = get_object_or_404(Post, pk=pk)
+   return render(request, "vlog/post_detail.html", {"post" : post})
+
+class PostUpdate(UpdateView):
+   model = Post
+   form_class = PostForm
+   template_name = "vlog/post_form.html"
+
+
+   def get_success_url(self):
+       return reverse_lazy("post_detail", kwargs={"pk": self.object.pk})
+
+
+class PostCreate(CreateView):
+   model = Post
+   form_class = PostForm
+   template_name = "vlog/post_form.html"
+   success_url = reverse_lazy("post_list")
+
+
+
